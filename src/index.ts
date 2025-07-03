6fd1585887148df526bbed4cd113c8dd561219e3
Chanbin Park <chanbin.park@berkeley.edu>
2025-07-03 21:44:30 +0900

    Pass schema setting registry to main panel

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,17 @@ import {
 } from '@jupyterlab/application';
 import { NotebookPanel } from '@jupyterlab/notebook';
 import { ILabShell } from '@jupyterlab/application';
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
 import { MainPanelWidget } from './components/mainpanelWidget';
 
 const extension: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab-a11y-fix',
+  id: 'jupyterlab-a11y-checker:plugin',
   autoStart: true,
-  requires: [ILabShell],
-  activate: (app: JupyterFrontEnd, labShell: ILabShell) => {
-    const panel = new MainPanelWidget();
+  requires: [ILabShell, ISettingRegistry],
+  activate: (app: JupyterFrontEnd, labShell: ILabShell, settingRegistry: ISettingRegistry) => {
+    console.log('JupyterLab extension jupyterlab-a11y-checker is activated!');
+
+    const panel = new MainPanelWidget(settingRegistry);
 
     labShell.add(panel, 'right');
 
